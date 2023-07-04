e305fcb8a0cbd666fa649e6337001153c88c7761
dev-mkr <webdev.mu@gamil.com>
2023-07-04 04:24:27 +0300

    change theme

--- src/components/Layout/AuthLayout.tsx
+++ src/components/Layout/AuthLayout.tsx
@@ -9,7 +9,7 @@ type PropsType = {
 
 const AuthLayout = ({ children, headingText }: PropsType) => {
   return (
-    <main className=" relative flex h-screen max-h-screen flex-col gap-y-10   sm:justify-evenly lg:gap-y-16">
+    <main className=" relative flex min-h-screen  flex-col gap-y-10   sm:justify-evenly lg:gap-y-16">
       <AuthHeader />
       <h1 className="pt-12 text-center text-3xl font-semibold tracking-tight sm:text-5xl sm:max-lg:pt-20">
         {headingText}
